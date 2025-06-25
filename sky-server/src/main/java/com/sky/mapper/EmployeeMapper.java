3e6ffbec445cdc85bd9452eee6495dff2b434c38
LAIZISHENYUAN <huajijunbilibili@gmail.com>
2025-06-25 15:21:22 +0800

    编辑员工业务代码

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -46,4 +46,14 @@ public interface EmployeeMapper {
      *
      * */
     void update(Employee employee);
+
+    /**
+     * 根据id查询员工信息
+     *
+     * @param id
+     * @return
+     *
+     * */
+    @Select("SELECT * FROM  employee WHERE id = #{id}")
+    Employee getById(Long id);
 }
