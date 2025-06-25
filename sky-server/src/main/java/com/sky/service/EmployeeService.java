1db6dd7104a25925c16c1665e36e3bc3ea0c3f8f
LAIZISHENYUAN <huajijunbilibili@gmail.com>
2025-06-25 10:18:41 +0800

    新增员工业务代码

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -1,7 +1,14 @@
 package com.sky.service;
 
+import com.sky.constant.PasswordConstant;
+import com.sky.constant.StatusConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
+import org.springframework.beans.BeanUtils;
+import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
 
 public interface EmployeeService {
 
@@ -12,4 +19,12 @@ public interface EmployeeService {
      */
     Employee login(EmployeeLoginDTO employeeLoginDTO);
 
+
+    /**
+     * 新增员工
+     *
+     * @param employeeDTO
+     *
+     */
+    void save(EmployeeDTO employeeDTO);
 }
